ab6f38e11c4dfc90e5223a38a171e35838b15ced
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-13 18:38:14 -0600

    basic router added 	new file:   app/controllers/router.js 	renamed:    app/home.html -> app/views/home.html

---
+++ app/controllers/router.js
@@ -0,0 +1,43 @@
+"use strict";
+const express = require('express');
+const path = require("path");
+const data_handler = require("./data_handler")
+const router = express.Router();
+//const homeRouter = require("./../routes/home");
+const productRouter = require("../routes/products")
+const adminProductRouter = require("../routes/admin_products")
+
+function validateAdmin(req, res, next) {
+  //console.log(req.headers['x-auth']);
+  let xAuth = req.headers['x-auth'];
+  if (xAuth === "admin") next();
+  if (xAuth != undefined) {
+    return;
+    res.status(403).send("");
+  }
+  else if (xAuth != "admin") {
+    return;
+    res.status(403).send("");
+  }
+  next();
+}
+
+router.use('/products', productRouter);
+router.use('/admin/products', validateAdmin, adminProductRouter);
+//router.use('/admin/products', adminProductRouter);
+
+router.route("/").get((req, res) =>
+  res.sendFile(path.join(__dirname, "../", "views", "home.html"))
+);
+router.route("/home").get((req, res) =>
+  res.sendFile(path.join(__dirname, "../", "views", "home.html"))
+);
+router.route("/home").get((req, res) =>
+  res.sendFile(path.join(__dirname, "../", "views", "leaderboard.html"))
+);
+
+router.route("/shopping_cart").get((req, res) =>
+  res.sendFile(path.join(__dirname, "../", "views", "shopping_cart.html"))
+);
+
+module.exports = router;
