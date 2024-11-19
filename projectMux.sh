39ec9c02dabb460ad32488e22cf0571591aee455
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-19 16:36:34 -0600

    modified:   projectMux.sh

--- projectMux.sh
+++ projectMux.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
-tmux new-session -d 
+SESSION_NAME="final_web"
+
+# Check if the session already exists
+if tmux has-session -t $SESSION_NAME 2>/dev/null; then
+    echo "Session $SESSION_NAME already exists. Attaching to it."
+    tmux attach-session -t $SESSION_NAME
+else
+    # Create a new session and name it
+    tmux new-session -d -s $SESSION_NAME
+    tmux new-window
+    tmux send-keys -t 1 'echo "Hello from pane 1"' C-m
+    tmux send-keys -t 0 'node server.js' C-m
+
+
+    # Split the window horizontally
+    #tmux split-window -h
+
+    # Send a command to the first pane
+    #
+
+    # Send a command to the second pane
+    #tmux send-keys -t 1 'echo "Hello from pane 2"' C-m
+
+    # Attach to the created session
+    tmux attach-session -t $SESSION_NAME
+fi
