79e53a631bb83d99709c91b9df37a20bfbcc3536
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-20 13:30:39 -0600

    removed hrefs from navbar, all is managed from onclisk in forceLogin. 	modified:   src/js/forceLogin.js 	modified:   views/home.html

--- app/src/js/forceLogin.js
+++ app/src/js/forceLogin.js
@@ -1,7 +1,15 @@
 "use strict";
-
-let leaderboardA = document.getElementById("leaderboard");
+console.log("hi");
+//let leaderboardA = document.getElementById("leaderboard");
 if (sessionStorage.getItem("loggedIn") != null) sessionStorage.setItem("loggedIn", false);
-window.location.href
-leaderboardA.onclick = ()
+
+let home = document.getElementById("home");
+let login = document.getElementById("login");
+let create = document.getElementById("create");
+let view = document.getElementById("view");
+let leaderboard = document.getElementById("leaderboard");
+
+leaderboard.onclick = (
+  () => { window.location.href = "stub";}  
+  );
 
