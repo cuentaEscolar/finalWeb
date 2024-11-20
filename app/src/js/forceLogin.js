d626bf68fadc34494aedfedd9cc925e1acd3aaad
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-20 13:01:44 -0600

    forceLogin se asegura de que estemos loggeados. esto no es una super buena manera de hacerlo pero eh 	new file:   app/src/js/forceLogin.js 	modified:   app/views/home.html

---
+++ app/src/js/forceLogin.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let leaderboardA = document.getElementById("leaderboard");
+if (sessionStorage.getItem("loggedIn") != null) sessionStorage.setItem("loggedIn", false);
+window.location.href
+leaderboardA.onclick = ()
+
