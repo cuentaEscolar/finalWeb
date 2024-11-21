5a817e7908fbe094d5ccf4a8c8bb032382992b4a
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-21 16:55:58 -0600

    home page will show ur user if logged in, else will ask u to login for all that is sacred 	modified:   app/src/js/forceLogin.js 	modified:   app/views/home.html

--- app/src/js/forceLogin.js
+++ app/src/js/forceLogin.js
@@ -8,23 +8,40 @@ let view = document.getElementById("view");
 let play = document.getElementById("play");
 let leaderboard = document.getElementById("leaderboard");
 
+let profile = document.getElementsByClassName("profile").item(0);
+let usedToHave = `
+            <h2 class="card-title h4" id="username">Tu Cuenta</h2>
+            <div class="bg-dark p-3 rounded mb-3 login-box" id="box">
+              <li>Rewrote the JS to generate cleaner code</li>
+              <li>Rewrote the CSS</li>
+              <li>Added new features!</li>
+              </ul>
+            </div>
+            <h3 class="h5">Other Tools</h3>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-light">HTML Cheatsheet</a></li>
+              <li><a href="#" class="text-light">Webmaster Links</a></li>
+              <li><a href="#" class="text-light">Site Ideas</a></li>
+            </ul>
+`
+profile.innerHTML = "Not Logged In\n";
 let hrefer = ((str) => { window.location.href = str; });
 let stubber = (() => { window.location.href = "stub"; });
 
 let ifLoggedIn = ((str) => {
-  console.log(str);
-  if (sessionStorage.getItem("loggedIn") === "false") {
-    stubber();
-  } else {
-    hrefer(str);
-  }
+	console.log(str);
+	if (sessionStorage.getItem("loggedIn") === "false") {
+		stubber();
+	} else {
+		hrefer(str);
+	}
 });
 console.log(sessionStorage.getItem("loggedIn"));
 console.log(sessionStorage.getItem("loggedIn") === "false");
 
 if (sessionStorage.getItem("loggedIn") === null || sessionStorage.getItem("loggedIn") === "false") {
-  console.log("wat");
-  sessionStorage.setItem("loggedIn", "false");
+	console.log("wat");
+	sessionStorage.setItem("loggedIn", "false");
 }
 
 create.addEventListener("click", (e) => ifLoggedIn("create"));
