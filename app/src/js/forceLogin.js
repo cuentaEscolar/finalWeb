cd564cb4c3157f47d0f7f66d4feffeb15c4edfef
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-20 19:52:00 -0600

    sessionStorage works properly now

--- app/src/js/forceLogin.js
+++ app/src/js/forceLogin.js
@@ -7,17 +7,28 @@ let create = document.getElementById("create");
 let view = document.getElementById("view");
 let play = document.getElementById("play");
 let leaderboard = document.getElementById("leaderboard");
+
+let hrefer = ((str) => { window.location.href = str; });
 let stubber = (() => { window.location.href = "stub"; });
 
+let ifLoggedIn = ((str) => {
+  console.log(str);
+  if (sessionStorage.getItem("loggedIn") === "false") {
+    stubber();
+  } else {
+    hrefer(str);
+  }
+});
 console.log(sessionStorage.getItem("loggedIn"));
-if (sessionStorage.getItem("loggedIn") === null || sessionStorage.getItem("loggedIn") === "false ") {
+console.log(sessionStorage.getItem("loggedIn") === "false");
+
+if (sessionStorage.getItem("loggedIn") === null || sessionStorage.getItem("loggedIn") === "false") {
   console.log("wat");
-
   sessionStorage.setItem("loggedIn", "false");
-  create.onclick = stubber;
-  view.onclick = stubber;
-  play.onclick = stubber;
-  leaderboard.onclick = stubber;
-
 }
+
+create.addEventListener("click", (e) => ifLoggedIn("create"));
+view.addEventListener("click", (e) => ifLoggedIn("view"));
+play.addEventListener("click", (e) => ifLoggedIn("play"));
+leaderboard.addEventListener("click", (e) => ifLoggedIn("leaderboard"));
 
