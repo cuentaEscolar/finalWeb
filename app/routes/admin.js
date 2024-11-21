b02e50d84fb32fc0491fe1ebbb8e26e5bcc35a51
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-21 15:34:44 -0600

    new file:   app/routes/admin.js

---
+++ app/routes/admin.js
@@ -0,0 +1,7 @@
+"use strict";
+const express = require('express');
+const path = require("path");
+const router = express.Router();
+router.route("/users")
+router.route("/users:uuid")
+module.exports = router;
