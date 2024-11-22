27942ae0842e5cee807f34d78d489bbb58b008e4
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-21 18:50:35 -0600

    modified:   app/routes/leaderboard.js

--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -5,7 +5,7 @@ const path = require("path");
 const router = express.Router();
 
 router.route("/").get((req, res) =>
-  res.sendFile(path.join(__dirname, "../", "views", "create.html"))
+  res.sendFile(path.join(__dirname, "../", "views", "leaderboard.html"))
 );
 
 module.exports = router;
