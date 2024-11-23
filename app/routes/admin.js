f628c00c12914187fbe56b46a4de94b35c0fbfaa
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-22 18:06:11 -0600

    now admins can update users by uuid and email 	modified:   routes/admin.js

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,6 +5,10 @@ const user_utils = require("../controllers/user_utils")
 const router = express.Router();
 
 router.route("/users").get((req, res) => user_utils.getUsers(req, res));
-router.route("/users/uuid/:uuid").get((req, res) => user_utils.getUsersByUuid(req, res));
-router.route("/users/email/:email").get((req, res) => user_utils.getUsersByEmail(req, res));
+router.route("/users/uuid/:uuid")
+	.get((req, res) => user_utils.getUsersByUuid(req, res))
+;
+router.route("/users/email/:email")
+	.get((req, res) => user_utils.getUsersByEmail(req, res))
+	.put((req,res) => user_utils.updateUserBy["email"](req,res));
 module.exports = router;
