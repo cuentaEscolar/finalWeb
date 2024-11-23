2b06d43d6452c2400ab04cc823d4ed3db8f30924
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-23 17:54:54 -0600

    modified:   routes/games.js

--- app/routes/games.js
+++ app/routes/games.js
@@ -4,4 +4,9 @@ const path = require("path");
 const dataHandler = require("../controllers/data_handler");
 const router = express.Router();
 
+
+router.route("/").get((req, res) =>
+  res.sendFile(path.join(__dirname, "../", "views", "game.html"))
+);
+
 module.exports = router;
