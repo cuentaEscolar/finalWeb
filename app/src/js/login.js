7b5fa16354d32e382a359a3a986e0e55deaa0961
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-23 13:56:49 -0600

    successfully loaded  login.js 	modified:   app/src/js/login.js 	modified:   app/views/login.html

--- app/src/js/login.js
+++ app/src/js/login.js
@@ -1,11 +1,12 @@
 "use strict";
 
-let login_button =  document.getElementById("login_button"); 
+let login_button = document.getElementById("login_button");
 let email = document.getElementById("email");
-let password = document.getElementById("password"); 
+let password = document.getElementById("password");
+console.log("logged");
 
-function login_request(){
-	console.log(email.value);
-	console.log(password.value);
+function login_request() {
+  console.log(email.value);
+  console.log(password.value);
 }
 login_button.addEventListener("click", login_request);
