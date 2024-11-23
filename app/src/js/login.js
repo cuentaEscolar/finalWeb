3a86244af5d1bbbf0987b65878e8b8599e87c095
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-23 13:14:31 -0600

    added stub login function 	modified:   src/js/login.js 	modified:   views/login.html

--- app/src/js/login.js
+++ app/src/js/login.js
@@ -1,3 +1,11 @@
 "use strict";
 
-let  
+let login_button =  document.getElementById("login_button"); 
+let email = document.getElementById("email");
+let password = document.getElementById("password"); 
+
+function login_request(){
+	console.log(email.value);
+	console.log(password.value);
+}
+login_button.addEventListener("click", login_request);
