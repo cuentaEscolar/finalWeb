9b0ff614755982daeedda028a78235023e0c8d4f
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-24 16:02:35 -0600

    tableGen works now 	modified:   src/js/game.js 	modified:   src/js/tableGen.js 	modified:   views/create.html

--- app/src/js/game.js
+++ app/src/js/game.js
@@ -2,7 +2,7 @@
 const utils = require("./utils.js");
 const generateUUID = utils.generateUUID;
 class Game {
-  constructor() {
+  constructor(rows, cols, title, topics, questions, answers, scores, uuid, creatorUuid) {
     this.rows = 0;
     this.cols = 0;
     this.title = "";
@@ -13,5 +13,21 @@ class Game {
     this.uuid = generateUUID();
     this.creatorUuid = -1;
   }
+  static getFields(){
+    return ["rows", "cols", "title", "topics", "questions", "answers", "scores", "uuid", "creatorUuid"];
+  }
+  static guestDefault() {
+    rows = 3;
+    rows = 1;
+    this.cols = 0;
+    this.title = "";
+    this.topics = [];
+    this.questions = [];
+    this.answers = [];
+    this.scores = [];
+    this.uuid = generateUUID();
+    this.creatorUuid = -1;
+  }
+
 }
 module.exports = { Game };
--- app/src/js/tableGen.js
+++ app/src/js/tableGen.js
@@ -1,7 +1,6 @@
 'use strict';
 
 let game = {
-
   rows: 0,
   cols: 0,
   title: "",
@@ -10,7 +9,10 @@ let game = {
   answers: [],
   scores: [],
 }
-
+const getX = (x) => sessionStorage.getItem(x);
+if (getX("authToken")==="false" || getX("authToken") == null){
+  window.location.href =  "/stub";
+}
 // Aquí obten el input de titulo
 let title = document.getElementById("titleInput");
 // Aquí busca y guarda el elemento del botón de Generar Tablero
@@ -31,14 +33,14 @@ let theadRow = thead.childNodes[1].childNodes;
 // almacenará la celda actual (cuando den clic en algún link a editar)
 let cell;  // esta variable se usará después en la función de requestData()
 
-function create_thead(cols){
+function create_thead(cols) {
   let str = `<thead>
                   <tr>
                     <th>Valores </th>
-   `; 
+   `;
   for (let index = 0; index < cols; index++) {
     str += `
-                    <th><a href="#" data-toggle="modal" data-target="#gameSummaryModal">Tema${index+1}</a></th>
+                    <th><a href="#" data-toggle="modal" data-target="#gameSummaryModal">Tema${index + 1}</a></th>
     `;
   }
 
@@ -48,24 +50,24 @@ function create_thead(cols){
   `
   return str;
 }
-function create_td(rows, cols){
+function create_td(rows, cols) {
   let s = `
-                    <td><a href="#" data-toggle="modal" data-target="#gameSummaryModal">Pregunta:Tema${cols}-${(rows+1)*100}</a></td>
+                    <td><a href="#" data-toggle="modal" data-target="#gameSummaryModal">Pregunta:Tema${cols}-${(rows + 1) * 100}</a></td>
 `
   return s;
 }
 
-function create_tbody(rows, cols){
-let str = `
+function create_tbody(rows, cols) {
+  let str = `
                 <tbody>
                 `
-  for (let index = 0; index < rows ; index++) {
+  for (let index = 0; index < rows; index++) {
     str += `
                   <tr>
-                    <td scope="row">${(index+1)*100}</td>
+                    <td scope="row">${(index + 1) * 100}</td>
     `
-    for (let jndex = 0; jndex < cols; jndex++){
-        str += create_td(index, jndex);
+    for (let jndex = 0; jndex < cols; jndex++) {
+      str += create_td(index, jndex);
     }
     str += `
                   </tr>
@@ -76,11 +78,11 @@ let str = `
   `
   return str;
 }
-function fillTable(rows, cols){
+function fillTable(rows, cols) {
   let generatedTable = create_thead(cols);
   generatedTable += create_tbody(rows, cols);
   mainTable.innerHTML = generatedTable;
-  
+
 }
 
 
@@ -113,19 +115,27 @@ const saveGame = (() => {
 
   // crea la matriz de preguntas en el objeto game
   game.questions = [];
+  game.answers = [];
+  game.scores = [];
   tbody.childNodes.forEach((element) => {
     if (element.nodeName === "#text") return;
     if (element.hidden) return;
     //element.setAttribute("style", `nth-of-type(${});`);
     let questions_ = [];
+    let answers_ = [];
+    let scores_ = [];
     let first = true;
     element.childNodes.forEach((element2) => {
       if (element2.nodeName === "#text") return;
       if (first) { first = false; return; }
       if (element2.hidden) return;
       questions_.push("");
+      scores_.push(0);
+      answers_.push("");
     });
     game.questions.push(questions_);
+    game.answers.push(answers_);
+    game.scores.push(scores_);
   });
 
 });
@@ -145,12 +155,13 @@ const getQuestion = (
   (topic_i, question_i) =>
     game.questions[topic_i][question_i]
 );
-const updateQuestion = (
+const updateGameAt = at =>  (topic_i, question_i) => newVal => game[at][topic_i][question_i] = newVal;
+const updateQuestion = updateGameAt("questions");/*(
   (topic_i, question_i) =>
   ((newVal) =>
     game.questions[topic_i][question_i] = newVal
   )
-);
+);*/
 
 const enableDisableButtons = (() => {
   if (title.value.length === 0) {
@@ -189,8 +200,8 @@ function generateGameBoard(event) {
   // selecciona los reglones usando nth-of-type(n+ algo ) y oculta
   // selecciona las columnas usando nth-of type(n+ algo ) para td y th
   console.log(`colsVal: ${colsVal}`);
-  fillTable(rowsVal,colsVal);
-  
+  fillTable(rowsVal, colsVal);
+
 
   // muestra la tabla (propiedad hidden)
   mainTable.removeAttribute('hidden');
@@ -198,8 +209,6 @@ function generateGameBoard(event) {
   // activar botón de generateJson
   setClass(btnJsonStr);
   return false;
-
-
   // regresa falso o usa
   // event.preventDefault();
   // return false;
@@ -215,10 +224,10 @@ let container = modalBody.childNodes[1];
 let question = container.getElementsByClassName("question")[0];
 let theme = container.getElementsByClassName("theme")[0];
 let json = container.getElementsByClassName("json")[0];
+let answer = container.getElementsByClassName("answer")[0];
 
 function requestData(event) {
   // filtrar, si no son tipo anchor Tag salirse de la función
-
   // actualiza la variable cell (que sea una celda tipo td o th)
   let isTd = false;
   cell = event.target.closest("td");
@@ -226,10 +235,10 @@ function requestData(event) {
   else cell = event.target.closest("th");
   if (!cell) return;
 
-  // console.log(container[0].childNodes);
   theme.setAttribute("hidden", "");
   question.setAttribute("hidden", "");
   json.setAttribute("hidden", "");
+  answer.setAttribute("hidden", "");
   // si están en un TH mostrar el modal solo la parte de la temática
   // Añade al valor el tema que se tiene guardado en el objeto
   let topic_i, question_i;
@@ -238,10 +247,9 @@ function requestData(event) {
     theme.removeAttribute("hidden");
     theme.getElementsByTagName("input")[0].textContent = game.topics[topic_i];
   } else {
-    // si están en un TD mostrar el modal solo la parte de la pregunta
-    // Muestra el Tema y el valor
-    // Muestra el valor de la pregunta
+
     question.removeAttribute("hidden");
+    answer.removeAttribute("hidden");
 
     question.getElementsByTagName("textarea")[0].value = getQuestion(cell.parentNode.rowIndex - 1, cell.cellIndex - 1);
   }
@@ -279,8 +287,12 @@ function curryPosition(topic_i, question_i) {
   function saveQuestion(event) {
     cell = mainTable.rows[topic_i].cells[question_i];
     cell.removeAttribute("class");
-    updateQuestion(topic_i - 1, question_i - 1)(question.getElementsByTagName("textarea")[0].value);
-    if (getQuestion(topic_i - 1, question_i - 1) != "") cell.setAttribute("class", "table-dark")
+    updateGameAt("questions")(topic_i - 1, question_i - 1)(question.getElementsByTagName("textarea")[0].value);
+    updateGameAt("answers")(topic_i - 1, question_i - 1)(answer.getElementsByTagName("textarea")[0].value);
+    updateGameAt("scores")(topic_i - 1, question_i - 1)((question_i)*100);
+    if (getQuestion(topic_i - 1, question_i - 1) != "") {
+      cell.setAttribute("class", "table-dark");
+    }
   }
   //  realiza las operaciones dependiendo en caso de pregunta o tema
   if (topic_i == 0) return saveTopic;
