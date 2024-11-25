755d9a3a5be334e898d7567dfe008d0858aa2277
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-25 09:29:44 -0600

    connecting signUp 	new file:   data/sillyCatUrls.txt 	new file:   src/js/signUp.js 	modified:   views/login.html 	modified:   views/signup.html

---
+++ app/src/js/signUp.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let userName = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let confirm_password = document.getElementById("confirm-password");
+console.log(userName,email,password,confirm_password);
