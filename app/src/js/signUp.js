8918680f2104d7be05deedf88ffdd76ab36f9618
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-25 12:35:32 -0600

    signUp added 	modified:   src/js/signUp.js

--- app/src/js/signUp.js
+++ app/src/js/signUp.js
@@ -9,26 +9,29 @@ let default_img = document.getElementById("default_img");
 let signUp_btn = document.getElementById("signUp_btn");
 
 const confirmMsg = () => {
-	if (confirm_password.value != password.value) {
-		confirm_password_msg.innerHTML = `<p style="color: red; ">passwords dont match</p>`;
-	} else {
-		confirm_password_msg.innerHTML = "";
-		name
-	}
+  if (confirm_password.value != password.value) {
+    confirm_password_msg.innerHTML = `<p style="color: red; ">passwords dont match</p>`;
+  } else {
+    confirm_password_msg.innerHTML = "";
+    name
+  }
 }
 confirm_password.addEventListener("keyup", confirmMsg);
 password.addEventListener("keyup", confirmMsg);
 
 signUp_btn.addEventListener("click", () => {
-	console.log(`default_img: ${parseInt(default_img.value)}`);
+  console.log(`default_img: ${parseInt(default_img.value)}`);
 
-	let user = { username: userName.value, email: email.value, password: password.value, role: "USER", default_img: parseInt(default_img.value) }
-	console.log("pressed :c");
-	genericCRUD("PUT")("/user")(
-		(user),
-		(x) => console.log("success"),
-		(x) => console.log("failure")
-	);
+  let user = { username: userName.value, email: email.value, password: password.value, role: "USER", default_img: parseInt(default_img.value) }
+  console.log("pressed :c");
+  genericCRUD("PUT")("/user")(
+    (user),
+    (x) => {
+      console.log(`${x}`);
+      window.location.href = "/login";
+    },
+    (x) => console.log("failure")
+  );
 }
 
 );
