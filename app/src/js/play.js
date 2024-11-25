c68d51f389e90dc848b3809cd846cc4933f731c7
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-24 23:32:29 -0600

    table gets generated correctly now yay 	modified:   src/js/play.js 	modified:   src/js/table_utils.js 	modified:   views/play.html

--- app/src/js/play.js
+++ app/src/js/play.js
@@ -10,12 +10,15 @@ function loadTable(x){
   let generatedTable = `
               <table class="table table-light mt-3" id="table" >
   `
-  generatedTable += create_thead(x.cols);
-  generatedTable += create_tbody( x.rows, x.cols);
+  generatedTable += create_thead_full(x.cols, x.topics);
+  generatedTable += create_tbody_full( x.rows, x.cols, x.topics);
   generatedTable += `
               </table>
   `
 
   table.innerHTML = generatedTable;
+}
+function runGame(){
+
 }
 loadBtn.addEventListener("click", ()=> loadTable(game));
--- app/src/js/table_utils.js
+++ app/src/js/table_utils.js
@@ -1,3 +1,20 @@
+function create_thead_full(cols, titles){
+  let str = `<thead>
+                  <tr>
+                    <th>Valores </th>
+   `;
+  for (let index = 0; index < cols; index++) {
+    str += `
+                    <th><a href="#" data-toggle="modal" data-target="#gameSummaryModal">${titles[index]}</a></th>
+    `;
+  }
+
+  str += `
+                  </tr>
+                </thead>
+  `
+  return str;
+}
 
 function create_thead(cols) {
   let str = `<thead>
@@ -16,11 +33,39 @@ function create_thead(cols) {
   `
   return str;
 }
+function create_td_topic(rows, cols, topic ){
+  let s = `
+                    <td><a href="#" data-toggle="modal" data-target="#gameSummaryModal">${topic} ${cols}-${(rows + 1) * 100}</a></td>
+`
+  return s;
+}
+
 function create_td(rows, cols) {
   let s = `
                     <td><a href="#" data-toggle="modal" data-target="#gameSummaryModal">Pregunta:Tema${cols}-${(rows + 1) * 100}</a></td>
 `
   return s;
+}
+function create_tbody_full(rows, cols, topics, ){
+  let str = `
+                <tbody>
+                `
+  for (let index = 0; index < rows; index++) {
+    str += `
+                  <tr>
+                    <td scope="row">${(index + 1) * 100}</td>
+    `
+    for (let jndex = 0; jndex < cols; jndex++) {
+      str += create_td_topic(index, jndex, topics[jndex] );
+    }
+    str += `
+                  </tr>
+    `
+  }
+  str += `
+                </tbody>
+  `
+  return str;
 }
 
 function create_tbody(rows, cols) {
