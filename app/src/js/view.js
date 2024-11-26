664d21ed34ee0fb266a6ef6c2b44847b0e4973b4
YOUR_NAME <lregaladofloriano@gmail.com>
2024-11-25 23:02:30 -0600

    view.js actually works now

--- app/src/js/view.js
+++ app/src/js/view.js
@@ -15,19 +15,19 @@ const make_td = x => {
   `
 
 }
-const saveGames = (x) =>{  sessionStorage.setItem("games", x); };
+const saveGames = (x) => { sessionStorage.setItem("games", x); };
 
 const getGamesFromUser = (user) => {
   // this shoudl look like docs
-  let id = {creatorUuid: user._id};   
+  let id = { creatorUuid: user._id };
   genericCRUD("GET")(`games/uuid/${id.creatorUuid}`)(id, saveGames, (x) => console.log("failure"));
 
-  let local_games = JSON.parse(sessionStorage.getItem("games"));
+  let local_games = JSON.parse(JSON.parse(sessionStorage.getItem("games"))[0]);
   return local_games;
-    
+
 }
 //make_tr :: User => Str
-const make_tr = (x) => (game) =>  {
+const make_tr = (x) => (game) => {
   return `<tr>
     ${make_td(0)}
     ${make_td(game.title)}
@@ -38,10 +38,11 @@ const make_tr = (x) => (game) =>  {
   `
 }
 const user = load_userInfo()
-const fill_table = ((x) => {
-  return x.map(make_tr(user)).reduce((acc,cur)=>acc+cur);
-});
+const fill_table = x =>
+  x.map(make_tr(user)).reduce((acc, cur) => acc + cur);
+;
 const user_games = getGamesFromUser(load_userInfo());
-load_games.addEventListener("click", (e)=>{
+console.log(user_games);
+load_games.addEventListener("click", (e) => {
   tableBody.innerHTML = fill_table(user_games);
 });
